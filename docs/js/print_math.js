5c0009586a2f1d9f9f8206f59c936f4fcddd9b04
apurvnakade <apurvnakade@gmail.com>
2022-01-03 18:42:17 -0600

    Update Day 01.

--- docs/js/print_math.js
+++
@@ -1,4 +0,0 @@
-export default function print_math(DOM_element_id, math_object, inline = false){
-  document.getElementById(DOM_element_id).innerHTML = "\\begin{align}" + math_object.print() + "\\end{align}";
-  MathJax.Hub.Queue(["Typeset",MathJax.Hub,DOM_element_id]);
-}
